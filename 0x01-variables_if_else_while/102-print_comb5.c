3439410d043ab6ec703985a18fd951a93fc0f4ae
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-02-17 21:24:51 +0200

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,30 +8,41 @@
  * All numbers should be printed with two digits. 1 should be printed as 01 .
  * The combination of numbers must be separated by comma, followed by a space.
  * The combinations of numbers should be printed in ascending order.
- * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1 .
+ * 00 01 & 01 00 are considered as a same combination of the numbers 0 and 1 .
  * Return: 0
  */
-int main(void) {
+int main(void)
+{
   int i, j;
 
-  for (i = 0; i <= 9; i++) {
-    for (j = 0; j <= 9; j++) {
-      if (i == 0 && j == 0) {
+  for (i = 0; i <= 9; i++)
+  {
+    for (j = 0; j <= 9; j++)
+    {
+      if (i == 0 && j == 0)
+      {
         putchar('0');
         putchar('0');
-      } else if (i == 0 && j != 0) {
+      }
+      else if (i == 0 && j != 0)
+      {
         putchar('0');
         putchar(j + '0');
-      } else if (i != 0 && j == 0) {
+      }
+      else if (i != 0 && j == 0)
+      {
         putchar(i + '0');
         putchar('0'); 
-      } else { 
+      }
+      else
+      { 
         putchar(i + '0');  
         putchar(j + '0'); 
 
       }
 
-      if (!(i == 9 && j == 9)) {
+      if (!(i == 9 && j == 9))
+      {
 	      printf(" , ");   
       }  
 
@@ -41,3 +52,4 @@ int main(void) {
 
   return (0);
 }
+

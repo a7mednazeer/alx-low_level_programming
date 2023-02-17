da98bcf493ca2f1ec16c780a244e4f64f8eb6f4d
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-02-17 21:11:57 +0200

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - main function to run code
+ * Description: prints all possible combinations of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01 .
+ * The combination of numbers must be separated by comma, followed by a space.
+ * The combinations of numbers should be printed in ascending order.
+ * 00 01 and 01 00 are considered as the same combination of the numbers 0 and 1 .
+ * Return: 0
+ */
 int main(void) {
   int i, j;
 
@@ -28,5 +39,5 @@ int main(void) {
 
   }
 
-  return 0;
+  return (0);
 }

1b78c67381ec0dbfd4756dd9b8ce9c11a1586023
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-02-17 19:41:26 +0200

    alx-low_level_programming

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/**
+/*
  * main - main function to run code
  * Description: prints the lowercase alphabet in reverse, followed by a new line.
  * starting from 0, followed by a new line.
@@ -10,13 +10,11 @@
 int main(void)
 {
 	char c = 'z';
-
 	while (c >= 'a')
 	{
 		putchar(c);
 		c--;
 	}
-
 	putchar('\n');
 	return (0);
 }

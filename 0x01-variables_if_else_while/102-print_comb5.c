aab6cec08d00e0050c86874e3ec8e8ac09a49d30
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-02-17 21:37:55 +0200

    alx-low_level_programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,42 @@
  */
 int main(void)
 {
+  int i, j;
 
-int i, j;
-
-    for (i = 0; i <= 99; i++)
+  for (i = 0; i <= 9; i++)
+  {
+    for (j = 0; j <= 9; j++)
     {
-        for (j = 0; j <= 99; j++)
-	{
-            if (i < j)
-	    {
-                printf("%02d %02d, ", i, j);
-            }
-	    else if (i > j)
-	    {
-                printf("%02d %02d, ", j, i); 
-            } 
-        } 
+      if (i == 0 && j == 0)
+      {
+        putchar('0');
+        putchar('0');
+      }
+      else if (i == 0 && j != 0)
+      {
+        putchar('0');
+        putchar(j + '0');
+      }
+      else if (i != 0 && j == 0)
+      {
+        putchar(i + '0');
+        putchar('0'); 
+      }
+      else
+      { 
+        putchar(i + '0');  
+        putchar(j + '0'); 
+
+      }
+
+      if (!(i == 9 && j == 9))
+      {
+	      printf(" , ");   
+      }  
+
     }
+
+  }
 
   return (0);
 }

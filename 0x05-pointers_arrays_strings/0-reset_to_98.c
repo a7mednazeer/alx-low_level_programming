66917e434f628e149ebe5fcfd94d5b919992f93a
Ahmed Nazeer <ahmed.m.nazeer.18@gmail.com>
2023-02-28 00:21:37 +0200

    alx-low_level_programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
  * reset_to_98 - tasks a pointer to an int and updates
  * the value it points to 98
- * 
+ *
  * @n: An integer input pointer
- * 
+ *
  * Return : nothing
  */
 void reset_to_98(int *n) 
